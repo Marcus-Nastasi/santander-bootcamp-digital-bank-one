66d1ec846a43db2a7cf90283bbe58d6eed0c153e
rolemberg <vinnie.nsts@gmail.com>
2024-05-10 20:23:38 -0300

    implemented structure on SavingsAccoun;

--- src/com/santander/digital/bank/Entities/Account/SavingsAccount.java
+++ src/com/santander/digital/bank/Entities/Account/SavingsAccount.java
@@ -1,4 +1,14 @@
 package com.santander.digital.bank.Entities.Account;
 
 public class SavingsAccount extends Account {
+
+    private static long SEQUENTIAL_CURRENT = 0;
+
+    public SavingsAccount(int agency) {
+        super.agency = agency;
+        super.id = SEQUENTIAL_CURRENT++;
+        super.balance = 0.0;
+    }
 }
+
+
