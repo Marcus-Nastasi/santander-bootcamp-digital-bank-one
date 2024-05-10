8abb83c67d59fe3f30065c93ff8ced88b49082a4
rolemberg <vinnie.nsts@gmail.com>
2024-05-10 20:06:06 -0300

    created IAccount structur;

--- src/com/santander/digital/bank/Entities/Account/IAccount.java
+++ src/com/santander/digital/bank/Entities/Account/IAccount.java
@@ -2,4 +2,8 @@ package com.santander.digital.bank.Entities.Account;
 
 public interface IAccount {
 
+    void withdraw(double value);
+    void deposit(double value);
+    void transfer(Account account, double value);
 }
+
