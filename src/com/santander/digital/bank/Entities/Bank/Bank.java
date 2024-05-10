1d7e22265d4fb7cc889907b7caaf0c45a1b36871
rolemberg <vinnie.nsts@gmail.com>
2024-05-10 20:00:57 -0300

    creating Bank class structure;

--- src/com/santander/digital/bank/Entities/Bank/Bank.java
+++ src/com/santander/digital/bank/Entities/Bank/Bank.java
@@ -1,4 +1,31 @@
 package com.santander.digital.bank.Entities.Bank;
 
+import com.santander.digital.bank.Entities.Account.Account;
+import com.santander.digital.bank.Entities.Client.Client;
+
+import java.util.ArrayList;
+
 public class Bank {
+
+    private String name;
+    private final ArrayList<Account> accounts = new ArrayList<>();
+    private final ArrayList<Client> clients = new ArrayList<>();
+
+    public Bank(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Account> getAccounts() {
+        return accounts;
+    }
+
+    public ArrayList<Client> getClients() {
+        return clients;
+    }
 }
+
+
