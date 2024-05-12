f425d525453dbd3990e76376610b5c34af024e83
rolemberg <vinnie.nsts@gmail.com>
2024-05-11 23:59:34 -0300

    testing on Main;

--- src/com/santander/digital/bank/Main.java
+++ src/com/santander/digital/bank/Main.java
@@ -2,7 +2,6 @@ package com.santander.digital.bank;
 
 import com.santander.digital.bank.Entities.Account.Account;
 import com.santander.digital.bank.Entities.Bank.Bank;
-import com.santander.digital.bank.Entities.Client.Client;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,7 +11,7 @@ public class Main {
             bank.getAccounts().forEach(a -> {
                 if (a.getBalance() == 0) a.deposit(100.00);
             });
-            //bank.getAccounts().forEach(System.out::println);
+            bank.getAccounts().forEach(System.out::println);
             bank.getAccounts().forEach(Account::bankStatement);
             bank.getClients().forEach(System.out::println);
         } catch (Exception e) {
