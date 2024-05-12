f3106a65aa38ef31c512bdc134b1938c2b6a4f19
rolemberg <vinnie.nsts@gmail.com>
2024-05-11 22:38:02 -0300

    added ClientException on Client class;

--- src/com/santander/digital/bank/Entities/Client/Client.java
+++ src/com/santander/digital/bank/Entities/Client/Client.java
@@ -1,15 +1,23 @@
 package com.santander.digital.bank.Entities.Client;
 
+import com.santander.digital.bank.Entities.Exceptions.ClientException;
+
 public class Client {
 
-    private long id;
-    private String name;
-    private String cpf;
+    private final long id;
+    private final String name;
+    private final String cpf;
 
-    public Client(long id, String name, String cpf) {
+    public Client(long id, String name, String cpf) throws ClientException {
+        if (name == null || cpf == null) throw new ClientException("Error: missing name or cpf");
         this.id = id;
         this.name = name;
         this.cpf = cpf;
+    }
+
+    @Override
+    public String toString() {
+        return("Client{" + "id=" + id + ", name='" + name + '\'' + ", cpf='" + cpf + '\'' + '}');
     }
 
     public long getId() {
@@ -18,11 +26,6 @@ public class Client {
 
     public String getName() {
         return name;
-    }
-
-    @Override
-    public String toString() {
-        return("Client{" + "id=" + id + ", name='" + name + '\'' + ", cpf='" + cpf + '\'' + '}');
     }
 }
 
