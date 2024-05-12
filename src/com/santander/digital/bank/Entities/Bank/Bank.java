3230645c5307d06dff5229d3f675ba3ca9586db0
rolemberg <vinnie.nsts@gmail.com>
2024-05-11 22:59:28 -0300

    creating init method on Bank;

--- src/com/santander/digital/bank/Entities/Bank/Bank.java
+++ src/com/santander/digital/bank/Entities/Bank/Bank.java
@@ -1,18 +1,22 @@
 package com.santander.digital.bank.Entities.Bank;
 
 import com.santander.digital.bank.Entities.Account.Account;
+import com.santander.digital.bank.Entities.Account.CurrentAccount;
 import com.santander.digital.bank.Entities.Client.Client;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Bank {
 
-    private String name;
+    private final String name;
     private final ArrayList<Account> accounts = new ArrayList<>();
     private final ArrayList<Client> clients = new ArrayList<>();
 
-    public Bank(String name) {
+    public Bank(String name) throws Exception {
         this.name = name;
+        this.init();
     }
 
     public String getName() {
@@ -25,6 +29,31 @@ public class Bank {
 
     public ArrayList<Client> getClients() {
         return clients;
+    }
+
+    public void init() throws Exception {
+        long id = 0;
+
+        List<Client> clients1 = Arrays.asList(
+            new Client(id++, "Gary Bean", "223-230-122-08"),
+            new Client(id++, "Jullie Nog", "223-230-122-08"),
+            new Client(id++, "Will Phill", "223-230-122-08"),
+            new Client(id++, "Lauren Gereth", "223-230-122-08"),
+            new Client(id++, "Shake Owen", "223-230-122-08"),
+            new Client(id++, "Mark Fostil", "223-230-122-08")
+        );
+
+        List<Account> accounts1 = Arrays.asList(
+            new CurrentAccount(1046, clients1.get(1)),
+            new CurrentAccount(1046, clients1.get(2)),
+            new CurrentAccount(1003, clients1.get(3)),
+            new CurrentAccount(1017, clients1.get(4)),
+            new CurrentAccount(1032, clients1.get(5)),
+            new CurrentAccount(1078, clients1.get(6))
+        );
+
+        this.clients.addAll(clients1);
+        this.accounts.addAll(accounts1);
     }
 }
 
